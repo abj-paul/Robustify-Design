fa4123ad19af5af8ee44e365fcea575ed934bd22
abj-paul <bsse1201@iit.du.ac.bd>
2025-01-29 17:14:12 +0600

    Fixed issue #38

--- pipeline/libs/execute_fortis.py
+++ pipeline/libs/execute_fortis.py
@@ -4,7 +4,7 @@ def run_fortis(project_path):
     os.chdir(project_path)
     print(os.getcwd())
 
-    jar_path = os.path.abspath(os.path.join("../../../pipeline/bin/fortis.jar"))
+    jar_path = os.path.abspath(os.path.join("../../bin/fortis.jar"))
     config_path = "config-pareto.json"
     os.system(f"java -jar {jar_path} robustify {config_path} --minimized")
 
